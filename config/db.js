131ce5f175f69da3c153309f1b8cd0448de8e0f9
Rohit Sharma <rohitsh1729@gmail.com>
2025-07-20 23:05:04 +0530

    test: debuggin

--- config/db.js
+++ config/db.js
@@ -2,11 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false // Required for Supabase
   }
