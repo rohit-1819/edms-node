47f77ce31f5b438ba2dd755fa5e6a17d2f74e3b4
Rohit Sharma <rohitsh1729@gmail.com>
2025-07-21 12:45:56 +0530

    ssl is not required

--- config/db.js
+++ config/db.js
@@ -3,9 +3,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Required for Supabase
-  }
+  ssl: false
 });
 
 module.exports = pool;
