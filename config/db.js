1efd8486d0d53a9ce7ee9dfa7781b13427ef8a79
Rohit Sharma <rohitsh1729@gmail.com>
2025-07-21 14:14:23 +0530

    feat: secure supabase connection with CA cert

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 
 require('dotenv').config();
 const { Pool } = require('pg');
-
+const path = require('path');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: true,
-    ca: fs.readFileSync('./prod-ca-2021.crt').toString(),
+    ca: fs.readFileSync(path.join(__dirname, '../certs/prod-ca-2021.crt')).toString(),
   },
 });
 
