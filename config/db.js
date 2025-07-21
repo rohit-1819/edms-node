7d09b0163543b293f2bb8e06faa57e4b22ce7833
Rohit Sharma <rohitsh1729@gmail.com>
2025-07-21 14:04:00 +0530

    feat: secure supabase connection with CA cert

--- config/db.js
+++ config/db.js
@@ -3,7 +3,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync('../prod-ca-2021.crt').toString(),
+  },
 });
 
 module.exports = pool;
