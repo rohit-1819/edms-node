7eff2c1f949f9c1af564547dbe12bb8a1d91a289
Rohit Sharma <rohitsh1729@gmail.com>
2025-07-21 16:45:16 +0530

    test: breaking database-url into multiple parts

--- config/db.js
+++ config/db.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const { Pool } = require('pg');
 const path = require('path');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT),
   ssl: {
-    rejectUnauthorized: false,
-  },
+    rejectUnauthorized: false // needed for Supabase SSL
+  }
 });
 
 module.exports = pool;
